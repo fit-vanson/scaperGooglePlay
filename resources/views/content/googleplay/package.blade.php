9f7d62d1120d322d33bbab65fe11c3ead1f5c566
fit-vanson <fit.vanson@gmail.com>
2021-08-25 10:04:32 +0700

    update

--- resources/views/content/googleplay/package.blade.php
+++ resources/views/content/googleplay/package.blade.php
@@ -385,19 +385,19 @@
     }
     var mySwiper9 = new Swiper('.swiper-coverflow', {
       effect: 'coverflow',
-      grabCursor: true,
-      centeredSlides: true,
-      slidesPerView: 'auto',
-      coverflowEffect: {
-        rotate: 50,
-        stretch: 0,
-        depth: 100,
-        modifier: 1,
-        slideShadows: true
-      },
-      pagination: {
-        el: '.swiper-pagination'
-      }
+      // grabCursor: true,
+      // centeredSlides: true,
+      // slidesPerView: 'auto',
+      // coverflowEffect: {
+      //   rotate: 50,
+      //   stretch: 0,
+      //   depth: 100,
+      //   modifier: 1,
+      //   slideShadows: true
+      // },
+      // pagination: {
+      //   el: '.swiper-pagination'
+      // }
     });
 
 
