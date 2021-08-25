da04269e5924f1ba493207316ca9744ded087f58
fit-vanson <fit.vanson@gmail.com>
2021-08-25 09:51:32 +0700

    update

--- resources/views/content/googleplay/package.blade.php
+++ resources/views/content/googleplay/package.blade.php
@@ -326,7 +326,7 @@
       rowData.screenshots.forEach(function(item, index, array) {
 
         a += '<div class="swiper-slide">'+
-                        '<img class="img-fluid screenshot_img" style= "max-width: none; height: 550px;padding: 0px 15px; display: inline-block" src="'+item+'" alt="screenshot" />'+
+                        '<img class="img-fluid screenshot_img" src="'+item+'" alt="screenshot" />'+
                 '</div>';
       })
       document.getElementById("screenshot_img").innerHTML = a;
