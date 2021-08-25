90de995d56dcde9f37a4b48726abac1af5237de9
fit-vanson <fit.vanson@gmail.com>
2021-08-25 10:12:01 +0700

    update

--- resources/views/content/googleplay/package.blade.php
+++ resources/views/content/googleplay/package.blade.php
@@ -388,13 +388,13 @@
       grabCursor: true,
       centeredSlides: true,
       slidesPerView: 'auto',
-      // coverflowEffect: {
-      //   rotate: 50,
-      //   stretch: 0,
-      //   depth: 100,
-      //   modifier: 1,
-      //   slideShadows: true
-      // },
+      coverflowEffect: {
+        rotate: 50,
+        stretch: 0,
+        depth: 100,
+        modifier: 1,
+        slideShadows: true
+      },
       pagination: {
         el: '.swiper-pagination'
       }
