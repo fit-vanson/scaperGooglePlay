96a0dd30ba14a61dc6e8577b3b4a862432a17e08
fit-vanson <fit.vanson@gmail.com>
2021-08-27 16:23:22 +0700

    update

--- app/Http/Controllers/GooglePlayController.php
+++ app/Http/Controllers/GooglePlayController.php
@@ -105,7 +105,7 @@ class GooglePlayController extends Controller
                                 </div>';
             if($record->checkExist != null){
                 if($record->checkExist->status == 1){
-                     $action .= '<div class="avatar avatar-status bg-light-warning">
+                     $action .= ' <div class="avatar avatar-status bg-light-warning">
                                     <span class="avatar-content">
                                     <a href="javascript:void(0)" onclick="unfollowApp('.$record->id.')" class="btn-flat-warning">
                                         <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-star"><polygon points="12 2 15.09 8.26 22 9.27 17 14.14 18.18 21.02 12 17.77 5.82 21.02 7 14.14 2 9.27 8.91 8.26 12 2"></polygon></svg>
