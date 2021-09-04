aa12374950058e2737a3b37e7e20ce18ac776091
fit-vanson <fit.vanson@gmail.com>
2021-09-04 11:08:44 +0700

    update

--- resources/views/layouts/contentLayoutMaster.blade.php
+++ resources/views/layouts/contentLayoutMaster.blade.php
@@ -13,6 +13,7 @@ $configData = Helper::applClasses();
 <head>
   <meta charset="utf-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests">
   <meta name="viewport" content="width=device-width,initial-scale=1.0">
   <meta name="csrf-token" content="{{ csrf_token() }}">
 
