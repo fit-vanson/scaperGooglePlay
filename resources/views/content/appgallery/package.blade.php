09ca1aab1fa6dba515a611b9d297b4bcc0201fb4
Administrator <fit.vanson@gmail.com>
2021-11-23 14:37:06 +0700

    app gallery

--- resources/views/content/appgallery/package.blade.php
+++ resources/views/content/appgallery/package.blade.php
@@ -241,8 +241,6 @@
         },
 
       ],
-      order: [[6, 'desc']],
-
       dom:
               '<"card-header border-bottom p-1"<"head-label"><"dt-action-buttons text-right"B>><"d-flex justify-content-between align-items-center mx-0 row"<"col-sm-12 col-md-6"l><"col-sm-12 col-md-6"f>>t<"d-flex justify-content-between mx-0 row"<"col-sm-12 col-md-6"i><"col-sm-12 col-md-6"p>>',
       buttons: [
